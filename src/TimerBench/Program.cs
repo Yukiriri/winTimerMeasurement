894cd198cac42fddb6699ac0b45101a12a4947cd
Yukiriri <50292962+Yukiriri@users.noreply.github.com>
2026-02-21 17:57:28 +0800

    改进代码

--- src/TimerBench/Program.cs
+++ src/TimerBench/Program.cs
@@ -1,62 +1,74 @@
-using System.Text;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Microsoft.Win32;
 
-Console.OutputEncoding = Encoding.UTF8;
-
-string cpuid;
+var cpuid = "";
 {
-    using var hkey = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0");
-    cpuid = hkey?.GetValue("ProcessorNameString").ToString() ?? "";
+    using var h_key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0");
+    if (h_key != null)
+        cpuid = h_key.GetValue("ProcessorNameString", "").ToString();
 }
 
-long qp_freq = 0;
+var qp_freq = 0ul;
 unsafe
 {
-    long qp_freq2 = 0;
-    PInvoke.QueryPerformanceFrequency(&qp_freq2);
+    var qp_freq2 = 0ul;
+    WinApi.QueryPerformanceFrequency((long*)&qp_freq2);
     qp_freq = qp_freq2;
 }
 
 Console.CursorVisible = false;
-for (;; Thread.Sleep(1000 / 30))
+for (;; Thread.Sleep(1))
 {
     Console.SetCursorPosition(0, 0);
 
     Console.WriteLine(cpuid + "\n");
     
-    Console.WriteLine($"{"WaitableTimer",15} : {"Request",12} | {"Actual",12}");
-    testWaitableTimer(1000 / 30.0);
-    testWaitableTimer(1000 / 60.0);
-    testWaitableTimer(1);
-    testWaitableTimer(0.5);
-    testWaitableTimer(0.001);
+    unsafe
+    {
+        const uint CREATE_WAITABLE_TIMER_HIGH_RESOLUTION = 0x2;
+        const uint TIMER_ALL_ACCESS = 0x1F0003;
+        var h_timers = new[]
+        {
+            ("WaitableTimer", WinApi.CreateWaitableTimer(bManualReset: false)),
+            ("WaitableTimerEx", WinApi.CreateWaitableTimerEx(
+                dwFlags: CREATE_WAITABLE_TIMER_HIGH_RESOLUTION, 
+                dwDesiredAccess: TIMER_ALL_ACCESS))
+        };
+        
+        foreach (var h_timer in h_timers)
+        {
+            Console.WriteLine($"{h_timer.Item1,-15} : {"Request",7+3} | {"Actual",7+3}");
+            if (h_timer.Item2 != HANDLE.Null)
+            {
+                testWaitableTimer(h_timer.Item2, 1000 / 30.0);
+                testWaitableTimer(h_timer.Item2, 1000 / 60.0);
+                testWaitableTimer(h_timer.Item2, 1);
+                testWaitableTimer(h_timer.Item2, 0.5);
+                testWaitableTimer(h_timer.Item2, 0.001);
+                WinApi.CloseHandle(h_timer.Item2);
+            }
+            Console.WriteLine();
+        }
+    }
 }
 
-unsafe void testWaitableTimer(double ms)
+unsafe void testWaitableTimer(HANDLE h_timer, double ms)
 {
-    const uint CREATE_WAITABLE_TIMER_HIGH_RESOLUTION = 0x2;
-    const uint TIMER_ALL_ACCESS = 0x1F0003;
-
-    var htimer = PInvoke.CreateWaitableTimerEx(null, null, CREATE_WAITABLE_TIMER_HIGH_RESOLUTION, TIMER_ALL_ACCESS);
-    if (htimer != HANDLE.Null)
+    for (var i = DateTimeOffset.Now.ToUnixTimeMilliseconds(); 
+         DateTimeOffset.Now.ToUnixTimeMilliseconds() - i < 1000;)
     {
-        for (var i = DateTimeOffset.Now.ToUnixTimeMilliseconds(); DateTimeOffset.Now.ToUnixTimeMilliseconds() - i < 1000;)
+        var ns = (long)-(ms * 1e+6 / 100);
+        if (WinApi.SetWaitableTimer(h_timer, &ns, 0, fResume: false))
         {
-            var ns = (long)-(ms * 1e+6 / 100);
-            if (PInvoke.SetWaitableTimer(htimer, &ns, 0, null, null, false))
+            ulong qpc1 = 0, qpc2 = 0;
+            WinApi.QueryPerformanceCounter((long*)&qpc1);
+            if (WinApi.WaitForSingleObject(h_timer, uint.MaxValue) == 0)
             {
-                long elapsed1 = 0, elapsed2 = 0;
-                PInvoke.QueryPerformanceCounter(&elapsed1);
-                if (PInvoke.WaitForSingleObject(htimer, uint.MaxValue) == 0)
-                {
-                    PInvoke.QueryPerformanceCounter(&elapsed2);
-                    Console.Write($"{"",15}   {ms,9:N3} ms | {(elapsed2 - elapsed1) * 1e+3 / qp_freq,9:N3} ms \r");
-                }
+                WinApi.QueryPerformanceCounter((long*)&qpc2);
+                Console.Write($"{"",-15}   {ms,7:N3} ms | {(qpc2 - qpc1) * 1e+3 / qp_freq,7:N3} ms \r");
             }
         }
-        Console.WriteLine();
-        PInvoke.CloseHandle(htimer);
     }
+    Console.WriteLine();
 }
--- src/TimerPrecision/Program.cs
+++ src/TimerPrecision/Program.cs
@@ -1,24 +1,22 @@
 using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using Windows.Win32;
 using Microsoft.Win32;
 
-Console.OutputEncoding = Encoding.UTF8;
-
-string cpuid;
+var cpuid = "";
 {
-    using var hkey = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0");
-    cpuid = hkey?.GetValue("ProcessorNameString").ToString() ?? "";
+    using var h_key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0");
+    if (h_key != null)
+        cpuid = h_key.GetValue("ProcessorNameString", "").ToString();
 }
 
-long qp_freq = 0;
+var qp_freq = 0ul;
 unsafe
 {
-    PInvoke.QueryPerformanceFrequency(&qp_freq);
+    WinApi.QueryPerformanceFrequency((long*)&qp_freq);
 }
 
 Console.CursorVisible = false;
-for (;; Thread.Sleep(1000 / 30))
+for (;; Thread.Sleep(1000 / 10))
 {
     Console.SetCursorPosition(0, 0);
 
@@ -28,62 +26,62 @@ for (;; Thread.Sleep(1000 / 30))
 
     unsafe
     {
-        long qpc1 = 0, qpc2 = 0;
-        PInvoke.QueryPerformanceCounter(&qpc1);
+        ulong qpc1 = 0, qpc2 = 0;
+        WinApi.QueryPerformanceCounter((long*)&qpc1);
         while (qpc1 >= qpc2)
-            PInvoke.QueryPerformanceCounter(&qpc2);
+            WinApi.QueryPerformanceCounter((long*)&qpc2);
         Console.WriteLine($"{(qpc2 - qpc1) * 1e+9 / qp_freq,9} ns : QueryPerformanceCounter");
 
         ulong qitp1 = 0, qitp2 = 0;
-        PInvoke.QueryInterruptTimePrecise(&qitp1);
+        WinApi.QueryInterruptTimePrecise(&qitp1);
         while (qitp1 >= qitp2)
-            PInvoke.QueryInterruptTimePrecise(&qitp2);
+            WinApi.QueryInterruptTimePrecise(&qitp2);
         Console.WriteLine($"{(qitp2 - qitp1) * 100,9} ns : QueryInterruptTimePrecise");
 
         ulong quitp1 = 0, quitp2 = 0;
-        PInvoke.QueryUnbiasedInterruptTimePrecise(&quitp1);
+        WinApi.QueryUnbiasedInterruptTimePrecise(&quitp1);
         while (quitp1 >= quitp2)
-            PInvoke.QueryUnbiasedInterruptTimePrecise(&quitp2);
+            WinApi.QueryUnbiasedInterruptTimePrecise(&quitp2);
         Console.WriteLine($"{(quitp2 - quitp1) * 100,9} ns : QueryUnbiasedInterruptTimePrecise");
 
         ulong stp1 = 0, stp2 = 0;
-        PInvoke.GetSystemTimePreciseAsFileTime((FILETIME*)&stp1);
+        WinApi.GetSystemTimePreciseAsFileTime((FILETIME*)&stp1);
         while (stp1 >= stp2)
-            PInvoke.GetSystemTimePreciseAsFileTime((FILETIME*)&stp2);
+            WinApi.GetSystemTimePreciseAsFileTime((FILETIME*)&stp2);
         Console.WriteLine($"{(stp2 - stp1) * 100,9} ns : GetSystemTimePreciseAsFileTime");
 
         Console.WriteLine();
 
         ulong qit1 = 0, qit2 = 0;
-        PInvoke.QueryInterruptTime(&qit1);
+        WinApi.QueryInterruptTime(&qit1);
         while (qit1 >= qit2)
-            PInvoke.QueryInterruptTime(&qit2);
+            WinApi.QueryInterruptTime(&qit2);
         Console.WriteLine($"{(qit2 - qit1) * 100 / 1e+6,9:N3} ms : QueryInterruptTime");
 
         ulong quit1 = 0, quit2 = 0;
-        PInvoke.QueryUnbiasedInterruptTime(&quit1);
+        WinApi.QueryUnbiasedInterruptTime(&quit1);
         while (quit1 >= quit2)
-            PInvoke.QueryUnbiasedInterruptTime(&quit2);
+            WinApi.QueryUnbiasedInterruptTime(&quit2);
         Console.WriteLine($"{(quit2 - quit1) * 100 / 1e+6,9:N3} ms : QueryUnbiasedInterruptTime");
 
-        ulong st1 = 0, st2 = 0;
-        PInvoke.GetSystemTimeAsFileTime((FILETIME*)&st1);
-        while (st1 >= st2)
-            PInvoke.GetSystemTimeAsFileTime((FILETIME*)&st2);
-        Console.WriteLine($"{(st2 - st1) * 100 / 1e+6,9:N3} ms : GetSystemTimeAsFileTime");
+        ulong gst1 = 0, gst2 = 0;
+        WinApi.GetSystemTimeAsFileTime((FILETIME*)&gst1);
+        while (gst1 >= gst2)
+            WinApi.GetSystemTimeAsFileTime((FILETIME*)&gst2);
+        Console.WriteLine($"{(gst2 - gst1) * 100 / 1e+6,9:N3} ms : GetSystemTimeAsFileTime");
 
         Console.WriteLine();
 
-        var gtc64_1 = PInvoke.GetTickCount64();
-        var gtc64_2 = 0ul;
+        ulong gtc64_1 = 0, gtc64_2 = 0;
+        gtc64_1 = WinApi.GetTickCount64();
         while (gtc64_1 >= gtc64_2)
-            gtc64_2 = PInvoke.GetTickCount64();
+            gtc64_2 = WinApi.GetTickCount64();
         Console.WriteLine($"{gtc64_2 - gtc64_1,9} ms : GetTickCount64");
 
-        var gtc_1 = PInvoke.GetTickCount();
-        var gtc_2 = 0ul;
+        uint gtc_1 = 0, gtc_2 = 0;
+        gtc_1 = WinApi.GetTickCount();
         while (gtc_1 >= gtc_2)
-            gtc_2 = PInvoke.GetTickCount();
+            gtc_2 = WinApi.GetTickCount();
         Console.WriteLine($"{gtc_2 - gtc_1,9} ms : GetTickCount");
 
         Console.WriteLine();
