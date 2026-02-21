fec662b970d485f49f68b8fd3dac7fb6ee268219
Yukiriri <50292962+Yukiriri@users.noreply.github.com>
2026-02-21 17:56:48 +0800

    修改TimerResolution实现

---
+++ src/TimerResolution/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Win32;
+using TimerResolution;
+
+var is_global_timer_request = false;
+{
+    using var h_key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\kernel");
+    if (h_key != null)
+        is_global_timer_request = (int)h_key.GetValue("GlobalTimerResolutionRequests", 0) == 1;
+}
+
+unsafe
+{
+    Console.WriteLine($"GlobalTimerResolutionRequests : {is_global_timer_request} \n");
+    
+    uint min_res = 0, max_res = 0, cur_res = 0;
+    WinApi.NtQueryTimerResolution(&max_res, &min_res, &cur_res);
+    Console.WriteLine($"""
+                       NtQueryTimerResolution :
+                            min : {min_res * 100 / 1e+6,7:N3} ms 
+                            max : {max_res * 100 / 1e+6,7:N3} ms
+                            cur : {cur_res * 100 / 1e+6,7:N3} ms
+                            
+                       """);
+
+    uint desired_res = 10, actual_res = 0;
+    WinApi.NtSetTimerResolution(desired_res, 1, &actual_res);
+    Console.WriteLine($"""
+                       NtSetTimerResolution :
+                            desired : {desired_res * 100 / 1e+6,7:N3} ms
+                            actual  : {actual_res * 100 / 1e+6,7:N3} ms
+                            
+                       """);
+}
+Console.WriteLine("Press any key to restore and exit...");
+Console.ReadKey();
---
+++ src/TimerResolution/WinApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TimerResolution;
+
+public static class WinApi
+{
+    [DllImport("ntdll.dll")]
+    public static extern unsafe int NtQueryTimerResolution(uint* MaximumTime, uint* MinimumTime, uint* CurrentTime);
+
+    [DllImport("ntdll.dll")]
+    public static extern unsafe int NtSetTimerResolution(uint DesiredTime, int SetResolution, uint* ActualTime);
+
+}
